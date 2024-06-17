7038aede884852a1273f021a430922380a558cde
Marifer26 <marifer26032003@gmail.com>
2024-06-16 23:31:25 -0600

    tarea
    codigos

--- parctica.2.4.Ordenamiento de árbol.py
+++ parctica.2.4.Ordenamiento de árbol.py
@@ -5,3 +5,49 @@ Carrera:Ingeniería Mecatrónica.
 Profesor : Mauricio Alejandro Cabrera Arellano 
 Materia:Inteligência Artificial 
 """
+class Node:
+    # Clase para crear nodos del árbol
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    # Función para insertar un nodo en el árbol
+    if root is None:
+        return Node(key)
+    else:
+        if root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+def inorder_traversal(root, sorted_list):
+    # Función para realizar el recorrido en orden del árbol y obtener el arreglo ordenado
+    if root:
+        inorder_traversal(root.left, sorted_list)
+        sorted_list.append(root.val)
+        inorder_traversal(root.right, sorted_list)
+
+def tree_sort(arr):
+    # Función principal para realizar el ordenamiento de árbol
+    if len(arr) == 0:
+        return []
+    
+    root = Node(arr[0])
+    
+    # Inserta todos los elementos del arreglo en el árbol
+    for i in range(1, len(arr)):
+        insert(root, arr[i])
+    
+    sorted_list = []
+    # Realiza el recorrido en orden para obtener el arreglo ordenado
+    inorder_traversal(root, sorted_list)
+    
+    return sorted_list
+
+# Ejemplo de uso del algoritmo de ordenamiento de árbol (Tree Sort)
+arr = [5, 4, 7, 2, 11]
+sorted_arr = tree_sort(arr)
+print("El arreglo ordenado es:", sorted_arr)
--- parctica.2.5.QuickSort.py
+++ parctica.2.5.QuickSort.py
@@ -5,3 +5,17 @@ Carrera:Ingeniería Mecatrónica.
 Profesor : Mauricio Alejandro Cabrera Arellano 
 Materia:Inteligência Artificial 
 """
+def quick_sort(arr):
+    # Función auxiliar para realizar el ordenamiento
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [x for x in arr[1:] if x <= pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+# Ejemplo de uso del algoritmo de QuickSort
+arr = [10, 7, 8, 9, 1, 5]
+sorted_arr = quick_sort(arr)
+print("El arreglo ordenado es:", sorted_arr)
