36cafc005b013f30e95a6f67f831619475b52cab
Marifer26 <marifer26032003@gmail.com>
2024-06-16 23:38:06 -0600

    tarea2.8
    codigo

--- parctica.2.8.Straight merging.py
+++ parctica.2.8.Straight merging.py
@@ -5,3 +5,34 @@ Carrera:Ingeniería Mecatrónica.
 Profesor : Mauricio Alejandro Cabrera Arellano 
 Materia:Inteligência Artificial 
 """
+def merge(arr1, arr2):
+    # Fusiona dos arreglos ordenados en un solo arreglo ordenado
+    sorted_arr = []
+    i = j = 0
+
+    # Recorre ambos arreglos y agrega el menor elemento al arreglo fusionado
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            sorted_arr.append(arr1[i])
+            i += 1
+        else:
+            sorted_arr.append(arr2[j])
+            j += 1
+
+    # Agrega los elementos restantes del primer arreglo si los hay
+    while i < len(arr1):
+        sorted_arr.append(arr1[i])
+        i += 1
+
+    # Agrega los elementos restantes del segundo arreglo si los hay
+    while j < len(arr2):
+        sorted_arr.append(arr2[j])
+        j += 1
+
+    return sorted_arr
+
+# Ejemplo de uso del algoritmo de mezcla directa (Straight Merging)
+arr1 = [1, 3, 5]
+arr2 = [2, 4, 6]
+sorted_arr = merge(arr1, arr2)
+print("El arreglo ordenado es:", sorted_arr)
