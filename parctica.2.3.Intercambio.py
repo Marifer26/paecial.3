3db61fb64f0e0ed80880c0e2ef949d42d93b066d
Marifer26 <marifer26032003@gmail.com>
2024-06-16 23:15:06 -0600

    practica2.3
    codigo

--- parctica.2.3.Intercambio.py
+++ parctica.2.3.Intercambio.py
@@ -1,1 +1,15 @@
+def bubble_sort(arr):
+    n = len(arr)
+    # Recorre todos los elementos del arreglo
+    for i in range(n):
+        # Los últimos i elementos ya están en su lugar
+        for j in range(0, n-i-1):
+            # Recorre el arreglo de 0 a n-i-1
+            # Intercambia si el elemento encontrado es mayor que el siguiente
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
+# Ejemplo de uso del algoritmo de ordenamiento por intercambio (Bubble Sort)
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("El arreglo ordenado es:", arr)
