2e0f624cbc7aae55d71964964ee7db396b8c3b53
Marifer26 <marifer26032003@gmail.com>
2024-06-16 23:10:23 -0600

    practica.2.2
    codigo

--- parctica.2.2.Selección (SelectionSort).py
+++ parctica.2.2.Selección (SelectionSort).py
@@ -1,1 +1,16 @@
+def selection_sort(arr):
+    # Recorre todos los elementos del arreglo
+    for i in range(len(arr)):
+        # Encuentra el mínimo en el resto del arreglo
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        
+        # Intercambia el mínimo encontrado con el primer elemento
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
+# Ejemplo de uso del algoritmo de ordenamiento por selección
+arr = [64, 25, 12, 22, 11]
+selection_sort(arr)
+print("El arreglo ordenado es:", arr)
